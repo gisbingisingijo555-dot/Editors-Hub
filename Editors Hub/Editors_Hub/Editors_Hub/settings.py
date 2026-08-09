6efdc66c9c12f9a46b0058c10f787d2ab2e9cca6
Gisbin Gisin Gijo <gisbingisingijo555@gmail.com>
2026-08-09 17:21:30 +0530

    Fix email configuration

--- Editors Hub/Editors_Hub/Editors_Hub/settings.py
+++ Editors Hub/Editors_Hub/Editors_Hub/settings.py
@@ -150,7 +150,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
 
 EMAIL_USE_TLS = True
